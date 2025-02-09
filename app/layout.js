c69c042318d1014c56489492f71665cfa4c56701
Akshatf <akshatfarkya7@gmail.com>
2025-02-09 11:49:06 +0530

    transaction page

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter ({subsets:["latin"]});
 
