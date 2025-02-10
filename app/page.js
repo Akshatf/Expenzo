21c17b66ccd5803dddad6d3536de7dd6f72618f7
Akshatf <akshatfarkya7@gmail.com>
2025-02-10 21:05:23 +0530

    updates

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ import Link from "next/link";
 
 const LandingPage = () => {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-white mt-40">
 
       <HeroSection />
 
