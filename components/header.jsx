6bd9e5a62d58be6d6f78cde164b746f04856ba16
Akshatf <akshatfarkya7@gmail.com>
2025-02-11 20:50:00 +0530

    updates

--- components/header.jsx
+++ components/header.jsx
@@ -1,30 +1,47 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
-import Link from "next/link";
+import React from "react";
 import { Button } from "./ui/button";
-import { LayoutDashboard, PenBox } from "lucide-react";
+import { PenBox, LayoutDashboard } from "lucide-react";
+import Link from "next/link";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { checkUser } from "@/lib/checkUser";
+import Image from "next/image";
 
 const Header = async () => {
   await checkUser();
 
   return (
-    <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
-      <nav className="container mx-autopx-4 py-4 flex items-center justify-between">
+    <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
         <Link href="/">
           <Image
             src={"/IMG-20250127-WA0066-removebg-preview.png"}
             alt="Expenzo Logo"
-            height={60}
             width={200}
+            height={60}
             className="h-12 w-auto object-contain"
           />
         </Link>
 
-        <div className="flex space-x-4 items-center">
+
+        <div className="hidden md:flex items-center space-x-8">
+          <SignedOut>
+            <a href="#features" className="text-gray-600 hover:text-blue-600">
+              Features
+            </a>
+            <a
+              href="#testimonials"
+              className="text-gray-600 hover:text-blue-600"
+            >
+              Testimonials
+            </a>
+          </SignedOut>
+        </div>
+
+
+        <div className="flex items-center space-x-4">
           <SignedIn>
             <Link
-              href={"/dashboard"}
+              href="/dashboard"
               className="text-gray-600 hover:text-blue-600 flex items-center gap-2"
             >
               <Button variant="outline">
@@ -32,15 +49,13 @@ const Header = async () => {
                 <span className="hidden md:inline">Dashboard</span>
               </Button>
             </Link>
-
-            <Link href={"/transaction/create"}>
+            <a href="/transaction/create">
               <Button className="flex items-center gap-2">
                 <PenBox size={18} />
                 <span className="hidden md:inline">Add Transaction</span>
               </Button>
-            </Link>
+            </a>
           </SignedIn>
-
           <SignedOut>
             <SignInButton forceRedirectUrl="/dashboard">
               <Button variant="outline">Login</Button>
@@ -57,7 +72,7 @@ const Header = async () => {
           </SignedIn>
         </div>
       </nav>
-    </div>
+    </header>
   );
 };
 
