ea53272cc2b2c0391beb2c1a9a12241e17fcc3ce
Akshatf <akshatfarkya7@gmail.com>
2025-02-16 17:40:14 +0530

    updates

--- app/layout.js
+++ app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
         <footer className="bg-blue-50 py-12">
           <div className="container mx-auto px-4 text-center text-grey-600">
             <p>
-              Made with 💗 by na baharwali na gharwali
+              Made with 💗
             </p>
           </div>
         </footer>
